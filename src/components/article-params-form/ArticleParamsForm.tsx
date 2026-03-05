37df997e19a0965e5c1870bfe157cc4d0e81711a
Varvara Mitusova <koltakovavarvara@gmail.com>
2026-03-05 16:48:53 +0300

    add components to ArticleParamsForm

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,58 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select';
+import { Text } from 'src/ui/text';
+
+import {
+	backgroundColors,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
 export const ArticleParamsForm = () => {
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
+			<ArrowButton isOpen={true} onClick={() => {}} />
 			<aside className={styles.container}>
 				<form className={styles.form}>
+					<Text size={31} weight={800} uppercase={true}>
+						Задайте параметры
+					</Text>
+					<Select
+						title='Шрифт'
+						selected={}
+						options={fontFamilyOptions}
+						onChange={}></Select>
+					<RadioGroup
+						title='Размер шрифта'
+						name='font-Size'
+						options={fontSizeOptions}
+						selected={}
+						onChange={}></RadioGroup>
+					<Select
+						title='Цвет шрифта'
+						selected={}
+						options={fontColors}
+						onChange={}></Select>
+
+					<Separator></Separator>
+
+					<Select
+						title='Цвет фона'
+						selected={}
+						options={backgroundColors}
+						onChange={}></Select>
+					<Select
+						title='Ширина контента'
+						selected={}
+						options={contentWidthArr}
+						onChange={}></Select>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
